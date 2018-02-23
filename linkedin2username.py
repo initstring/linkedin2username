f8a5bcc7ec2e1b12b9d4b9b2766454dc0604ef70
InitString <26131150+initstring@users.noreply.github.com>
2018-02-23 11:39:45 +1100

    made slightly better...

--- linkedin2username.py
+++ linkedin2username.py
@@ -39,15 +39,15 @@ class LinkedInParser(object):
         ]
 
         # Login
-        self.loginPage()
+        self.login_page()
 
-        title = self.loadTitle()
+        title = self.load_title()
         print title
 
         self.cj.save()
 
 
-    def loadPage(self, url, data=None):
+    def load_page(self, url, data=None):
         """
         Utility function to load HTML from URLs for us with hack to continue despite 404
         """
@@ -63,13 +63,13 @@ class LinkedInParser(object):
             # If URL doesn't load for ANY reason, try again...
             # Quick and dirty solution for 404 returns because of network problems
             # However, this could infinite loop if there's an actual problem
-            return self.loadPage(url, data)
+            return self.load_page(url, data)
 
-    def loginPage(self):
+    def login_page(self):
         """
         Handle login. This should populate our cookie jar.
         """
-        html = self.loadPage("https://www.linkedin.com/")
+        html = self.load_page("https://www.linkedin.com/")
         soup = BeautifulSoup(html)
         try:
             csrf = soup.find(id="loginCsrfParam-login")['value']
@@ -81,30 +81,61 @@ class LinkedInParser(object):
             'loginCsrfParam': csrf,
         })
 
-        html = self.loadPage("https://www.linkedin.com/uas/login-submit", login_data)
+        html = self.load_page("https://www.linkedin.com/uas/login-submit", login_data)
         return
 
-    def loadTitle(self):
-        html = self.loadPage("https://www.linkedin.com/feed/")
+    def load_title(self):
+        html = self.load_page("https://www.linkedin.com/feed/")
         soup = BeautifulSoup(html)
         return soup.find("title")
 
 parser = LinkedInParser(username, password)
 
-
-def scrapeInfo():
+def scrape_info():
     print('Starting search....')
     for page in range(0, searchDepth):
+        print('OK, looking for results on page ' + str(page+1))
+        fullNameList = []
         url = 'https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%22'+companyID+'%22%5D&page=' + str(page+1)
-        result = parser.loadPage(url)
+        result = parser.load_page(url)
         firstName = re.findall(r'firstName&quot;:&quot;(.*?)&', result)
         lastName = re.findall(r'lastName&quot;:&quot;(.*?)&', result)
         
         for first,last in zip(firstName,lastName):
-            print(first + ' ' + last)
+            fullName = first + ' ' + last
+            if fullName not in fullNameList:
+                fullNameList.append(fullName)
+    return fullNameList
+
+def clean(list):
+    cleanList = []
+    for name in list:
+        name = re.sub(r'[,(].*', '', name) # People have a habit of listing lame creds after their name. Gone!
+        name = name.strip()
+        if name not in cleanList:
+            cleanList.append(name)
+    return cleanList
+        
+def write_files(list):
+    rawnames = open('rawnames.txt', 'w')
+    flast = open('flast.txt', 'w')
+    firstl = open('firstl.txt', 'w')
+    firstlast = open('first.last.txt', 'w')
+    for name in list:
+        try:
+            rawnames.write(name + '\n')
+            parse = name.split(' ')
+            flast.write(parse[0][0] + parse[-1] + '\n')
+            firstlast.write(parse[0] + '.' + parse[-1] + '\n')
+            firstl.write(parse[0] + parse[-1][0] + '\n')
+        except:
+            print('Choked on ' + name + ' but continuing...')
 
 def main():
-    data = scrapeInfo()
+    fullNameList  = scrape_info()
+    cleanList = clean(fullNameList)
+    write_files(cleanList)
+    print('All done! Check out your lovely new files.')
 
 if __name__ == "__main__":
     main()
